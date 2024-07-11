5f3cd22345047a69decf8a9097e5c0ee1593958e
Kramm <134699029+InvaderKrm@users.noreply.github.com>
2024-07-11 11:29:52 -0300

    atv1

---
+++ import math.py
@@ -0,0 +1,4 @@
+import math
+real=float(input('Vamos converter um número real para inteiro com Python?\n::Insira o número real escolhido: '))
+inteiro=math.floor(real)
+print('O número',real,'convertido para inteiro é',inteiro)
