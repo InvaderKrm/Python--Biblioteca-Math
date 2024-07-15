94dae82b8c4632864f4519a593e497fe0e202fb3
Kramm <134699029+InvaderKrm@users.noreply.github.com>
2024-07-15 10:03:50 -0300

    atualização extra 2

--- EXTRA2.py
+++ EXTRA2.py
@@ -1,4 +1,4 @@
 import math
 nmrs = [i for i in range(0, 21, 2)]
-soma=sum(nmrs)
+soma=math.fsum(nmrs)
 print('Iremos somar a seguinte lista com Python:\n',nmrs,'\nA soma desses números é igual a',soma)
