227e7f59f7809d2370ebb2c97f72791d5e20f319
Kramm <134699029+InvaderKrm@users.noreply.github.com>
2024-07-15 09:57:04 -0300

    extras

--- ATIVIDADE2.py
+++ ATIVIDADE2.py
@@ -2,4 +2,4 @@ import math
 oposto=int(input('Cateto oposto: '))
 adjacente=int(input('Cateto adjacente: '))
 hipotenusa=math.sqrt(math.pow(oposto, 2)+math.pow(adjacente, 2))
-print('A hipotenusa vale: ', hipotenusa)
+print('A hipotenusa vale: {hipotenusa}')
---
+++ ATIVIDADE3.py
@@ -0,0 +1,7 @@
+import math
+angulo=int(input('Insira um ângulo: '))
+rad=math.radians(angulo)
+seno=math.sin(rad)
+cos=math.cos(rad)
+tan=math.tan(rad)
+print('Seno: {seno};\nCosseno: {cos};\nTangente: {tan}.')
